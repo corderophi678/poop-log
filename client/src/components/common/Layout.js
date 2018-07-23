debf37407456109387d9c6b50febe9922f70f61a
Dr. Rich Cordero <rich@corderophilosophy.com>
2018-07-23 11:19:12 -0400

    add contact via twitter link to layout

--- client/src/components/common/Layout.js
+++ client/src/components/common/Layout.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export function Layout({children}) {
+export function Layout({ children }) {
   return (
     <div className="mw9-ns mw6 center pt5 pt6-ns flex flex-column items-center justify-center ph2">
       {children}
+      <div className="self-center mt4">
+        <p className="b code f5">
+          Problems? Reach out via Twitter{' '}
+          <a href="https://twitter.com/corderophi678" target="_blank">@corderophi678</a>
+        </p>
+      </div>
     </div>
   )
 }
